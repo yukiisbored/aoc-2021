737088c743b158dc57387c75df2db83beb991a7d
Muhammad Kaisar Arkhan (Yuki) <hi@yukiisbo.red>
2021-12-04 14:09:03 +0100

    Add Day 4 part 2

--- 4/solve.hs
+++ 4/solve.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TupleSections #-}
 module Main where
 
 import Control.Applicative
@@ -6,18 +7,31 @@ import Data.Functor
 import Data.List
 import qualified Text.ParserCombinators.ReadP as P
 
+import Data.Bifunctor
+
 type Board = [[Int]]
 type Puzzle = ([Int], [Board])
 
+win xs y = let f = any (all (`elem` xs)) in  f y || f (transpose y)
+
 part1 :: Puzzle -> Int
 part1 (xs, bs) = n * b'
-  where f xs y = let f = any (all (`elem` xs)) in  f y || f (transpose y)
-        f' (xs, bs) x = let g = f xs in if any g bs then (xs, filter g bs) else (x:xs, bs)
+  where f' (xs, bs) x = let g = win xs in if any g bs then (xs, filter g bs) else (x:xs, bs)
         (ns@(n:_), [b]) = foldl f' ([], bs) xs
         b' = sum $ filter (not . (`elem` ns)) $ concat b
 
-part2 :: a -> Int
-part2 xs = undefined
+part2 :: Puzzle -> Int
+part2 (xs, bs) = b' * n
+  where f (xs, bzs) x = let xs' = x : xs
+                            win' = win xs'
+                            f (b, Nothing) = if win' b then (b, Just x) else (b, Nothing)
+                            f bz = bz
+                        in (xs', map f bzs)
+        ys = foldl f ([], map (, Nothing) bs) xs
+        ys' = map (second (>>= (`elemIndex` xs))) (snd ys)
+        (b, Just p) = maximumBy (\(_, a) (_, b) -> compare a b) ys'
+        b' = sum $ filter (not . (`elem` take (p + 1) xs)) $ concat b
+        n = xs !! p
 
 pp :: P.ReadP Puzzle
 pp = do
@@ -35,4 +49,4 @@ main = do
   let cnt' = cnt ++ "\n" -- Ugly hack, whatever.
       xs = head $ fst $ last $ P.readP_to_S (P.many pp) cnt'
 
-  print $ part1 xs
+  print (part1 xs, part2 xs)
